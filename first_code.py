a7f7ea7a948469d7f7775c47b6d353f4676bf1a4
Amir Aziz <134054687+AmirAziz1221@users.noreply.github.com>
2025-07-13 15:02:43 +0500

    Add files via upload

---
+++ first_code.py
@@ -0,0 +1,9 @@
+x = int(input("Enter first number: "))  # input from user
+y = int(input("Enter second number: "))  # input from user
+print(f"addition of {x} and {y} =", x+y  )  # addition
+
+
+if x<y: #conditional statement
+    print(f"{x} is less than {y}")
+else:
+    print(f"{x} is greater than {y}")
