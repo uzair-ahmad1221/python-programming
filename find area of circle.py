e01f7a235ad5ff6ce65d6fb6d42701839651c330
Uzair Ahmad Dawar <uzairahmadiiui1221@gmail.com>
2025-07-18 01:53:10 +0500

    area of circle from cm....grams

---
+++ find area of circle.py
@@ -0,0 +1,11 @@
+#find the area of larger circle
+A1=3.14*6*6
+#find the area of second circle
+A2=3.14*3*3
+#find the area of metal sheet to make a ring
+A3=A1-A2
+#find the weight of metal ring
+w=A3*10
+#answar W is in gms
+print("The weight of metal ring is:",w,"grams")
+
